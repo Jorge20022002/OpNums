3467ebef406f3b5d00bc095ed750299b5abdda91
HP <aluno81802@ipt.pt>
2022-06-15 10:52:01 +0100

    frist commit

--- OpNums/__init__.py
+++ OpNums/__init__.py
@@ -1,8 +1,8 @@
 from OpNums.mod1 import maxNum
 print(maxNum(4,5))
 
-from OpNums.mod1 import numlen
-print(numlen(5))
+from OpNums.mod1 import numLen
+print(numLen(5))
 
 from OpNums.mod2 import meanNum
 print(meanNum(4,5))
